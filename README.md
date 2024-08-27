173a96b76848d98589fb738f288c32ba32fe586e
connerdekok <connerdekok@gmail.com>
2024-08-26 23:55:49 -0400

    adding new finished files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SQL-Challenge
+
+Title- Homework Module 9 SQL Challenge- Conner Dekok
+
+In this assignment we used PgAdmin4 to help create a SQL script to create a variety of tables along with creating an EDR to explain the relationship between the tables we created.  I also answered a variety of questions based on the information given from the tables. 
+
+Collaboration with other students- I worked with Hunter Becker when it came to working on this project.  We worked together on completing this project and comparing the results we had gotten. 
+I reviewed the in-class meetings and reviewed the examples we went through in these class lessons. 
+I used Microsoft copilot to help correct or ask any questions I had when creating my code. 
+
+https://zoom.us/rec/play/TRekuhR8L4JiFWQAq--bmeGsWoXaX_isc6ZEAikJ0XK3VuJfCgYlXuNFQLlVKvZb7_iQsXq5IZn-wa3i.Rn-2C2uU9Vv0E5_b
+
+https://zoom.us/rec/play/5Xc8eCV8T9c_A99Q6q9tv2SOBtcHDfxlnsCQ5dMC-VVDTNK9jAOmy6l-hmbLmQq1Xe6Y0223pQNqKGCF.dADdWnw4DdzvK31v
+
+
---
+++ SQL-Project.sql
@@ -0,0 +1,56 @@
+
+--Create Tables from CSV And order them correctly.
+--Also input the CSV's in the order as they are listed here
+--Listed Values at 30 to be positive all data fits.
+
+CREATE TABLE titles (
+title_id VARCHAR(30) NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE departments (
+dept_no VARCHAR(30) NOT NULL PRIMARY KEY,
+dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+emp_no INT(30) NOT NULL PRIMARY KEY,
+emp_title_id VARCHAR(30) NOT NULL,
+birth_date DATE(30) NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,	
+sex	VARCHAR(30) NOT NULL,
+hire_date DATE(30) NOT NULL,
+FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+emp_no INT(30) NOT NULL PRIMARY KEY,
+salary INT(30) NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+dept_no VARCHAR NOT NULL,
+FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR NOT NULL,
+emp_no INT NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+);
+
+
+
+--Query each table to see if tables run.
+SELECT * from departments;
+SELECT * from dept_emp;
+SELECT * from dept_manager;
+SELECT * from employees;
+SELECT * from salaries;
+SELECT * from titles;
+
